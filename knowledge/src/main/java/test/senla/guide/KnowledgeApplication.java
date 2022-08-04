4397ccaec3bc1f6caf1f995f80029003b6c861cf
Zema128 <vladzemec@icloud.com>
2022-08-04 15:21:40 +0300

    Initial commit

---
+++ knowledge/src/main/java/test/senla/guide/KnowledgeApplication.java
@@ -0,0 +1,11 @@
+package test.senla.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeApplication.class, args);
+    }
+}
