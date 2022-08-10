bafee63b487193510a787772608983c8ef7df67a
Zema128 <vladzemec@icloud.com>
2022-08-10 16:58:28 +0300

    test commit

---
+++ knowledge/src/test/java/test/senla/guide/service/MajorThemeServiceImplTest.java
@@ -0,0 +1,10 @@
+package test.senla.guide.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MajorThemeServiceImplTest {
+
+}
